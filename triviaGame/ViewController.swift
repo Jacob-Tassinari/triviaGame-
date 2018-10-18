5ec93b6270d4d66f3252e53a2c1c558f49e57d77
Jacob Tassinari <jacobtassinari@Interapts-MacBook-Pro-3.local>
2018-10-18 10:01:37 -0500

    Initial Commit

---
+++ triviaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  triviaGame
+//
+//  Created by Jacob Tassinari on 10/18/18.
+//  Copyright © 2018 Jacob Tassinari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
